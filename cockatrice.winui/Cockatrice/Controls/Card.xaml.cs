c0b11eea830229f6611d1b0edb21cc56e7658348
Matt Jaeger <matt.jaeger@outlook.com>
2024-09-30 21:01:22 -0500

    WIP

---
+++ cockatrice.winui/Cockatrice/Controls/Card.xaml.cs
@@ -0,0 +1,40 @@
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Controls.Primitives;
+using Microsoft.UI.Xaml.Data;
+using Microsoft.UI.Xaml.Input;
+using Microsoft.UI.Xaml.Media;
+using Microsoft.UI.Xaml.Navigation;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+
+namespace Cockatrice.Controls
+{
+    public sealed partial class Card : UserControl
+    {
+        public Card()
+        {
+            this.InitializeComponent();
+        }
+
+        private void UserControl_DoubleTapped(object sender, DoubleTappedRoutedEventArgs e)
+        {
+
+        }
+
+        private void UserControl_Tapped(object sender, TappedRoutedEventArgs e)
+        {
+
+        }
+
+        private void UserControl_RightTapped(object sender, RightTappedRoutedEventArgs e)
+        {
+
+        }
+    }
+}
